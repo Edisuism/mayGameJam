30f229172cd7a007c24e4b0573a3443673e55cf7
Edisuism <53289506+Edisuism@users.noreply.github.com>
2020-05-22 21:48:54 +1000

    yes

---
+++ unitymayjam/Assets/Scripts/GameManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    //Set in inspector
+    public GameObject completeLevelUI;
+    public GameObject deathUI;
+
+    private GameObject player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    public void GameOver()
+    {
+        RemoveGhosts();
+        StartCoroutine("ShowDeathUI");
+        //TODO: Make character look forward, perform death animation
+    }
+
+    public void Win()
+    {
+        RemoveGhosts();
+        completeLevelUI.SetActive(true);
+        StartCoroutine("LoadMenu");
+    }
+
+    private void RemoveGhosts()
+    {
+        GameObject[] ghostArray = GameObject.FindGameObjectsWithTag("Ghost");
+        foreach(GameObject ghost in ghostArray)
+        {
+            Destroy(ghost);
+        }
+    }
+
+    IEnumerator LoadMenu()
+    {
+        yield return new WaitForSeconds(7.0f);
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    IEnumerator ShowDeathUI()
+    {
+        yield return new WaitForSeconds(7.6f);
+        deathUI.SetActive(true);
+        yield return new WaitForSeconds(0.05f);
+        deathUI.GetComponentInChildren<Image>().color = Color.black;
+        yield return new WaitForSeconds(3.0f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ unitymayjam/Assets/Scripts/Skeleton.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ColorType { red, green, blue, yellow, magenta, cyan, white }
+
+public class Skeleton : MonoBehaviour
+{
+    public ColorType colorType;
+    public GameObject pos1;
+    public GameObject pos2;
+    private bool goingPos1 = true;
+    private float speed = 1.0f;
+    private SpriteRenderer spriteRenderer;
+    private float SinTime { get { return Mathf.Sin(Time.time); } }
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        ColorGet(colorType);
+        
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.Lerp(pos1.transform.position, pos2.transform.position, (SinTime + 1f) * .5f);
+        spriteRenderer.flipX = Mathf.Cos(Time.time) < 0;
+        
+        /*
+        if (goingPos1)
+        {
+            transform.Translate(pos1.transform.position * speed * Time.deltaTime);
+        }
+        else
+        {
+            transform.Translate(pos2.transform.position * speed * Time.deltaTime);
+        }
+
+        if (transform.position == pos1.transform.position)
+        {
+            goingPos1 = false;
+        }
+
+        if (transform.position == pos2.transform.position)
+        {
+            goingPos1 = true;
+        }
+        */
+    }
+
+    private void ColorGet(ColorType color)
+    {
+        switch (color)
+        {
+            case ColorType.red:
+                spriteRenderer.color = Color.red;
+                break;
+            case ColorType.green:
+                spriteRenderer.color = Color.green;
+                break;
+            case ColorType.blue:
+                spriteRenderer.color = Color.blue;
+                break;
+            case ColorType.yellow:
+                spriteRenderer.color = Color.yellow;
+                break;
+            case ColorType.magenta:
+                spriteRenderer.color = Color.magenta;
+                break;
+            case ColorType.cyan:
+                spriteRenderer.color = Color.cyan;
+                break;
+            case ColorType.white:
+                spriteRenderer.color = Color.white;
+                break;
+        }
+    }
+}
