9b612d26bc8ba871216c8befa0e5728de4ec91d0
theducvu <vutheduc96@gmail.com>
2020-05-22 23:24:53 +1000

    2 levels + camera follow

---
+++ unitymayjam/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform FollowTarget;
+    public float SPEED;
+
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(FollowTarget.position.x, FollowTarget.position.y, -10f), SPEED);
+    }
+}
